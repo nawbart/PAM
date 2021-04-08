cb39798632c4b3b702e529101646c3432df83d1d
bartek <nawrockibartoszwro@gmail.com>
2021-04-08 18:46:24 +0200

    dodanie i ograniczenie ruchów stworka, którym się poruszamy.

--- app/src/main/java/com/example/pam_gra/MainActivity.java
+++ app/src/main/java/com/example/pam_gra/MainActivity.java
@@ -3,18 +3,32 @@ package com.example.pam_gra;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
     // Elements
     private TextView scoreLabel, startLabel;
-    private ImageView box, orange, pink, black;
+    private ImageView stworek, punkt, punkt2, bomba;
 
     private int boxY;
+
+
+    // Timer
+    private Timer timer = new Timer();
+    private Handler handler = new Handler();
+
+    // Status
+    private boolean action_flg = false;
+    private boolean start_flg = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,30 +38,87 @@ public class MainActivity extends AppCompatActivity {
 
         scoreLabel = findViewById(R.id.scoreLabel);
         startLabel = findViewById(R.id.startLabel);
-        box = findViewById(R.id.box);
-        orange = findViewById(R.id.orange);
-        pink = findViewById(R.id.pink);
-        black = findViewById(R.id.black);
+        stworek = findViewById(R.id.box);
+        punkt = findViewById(R.id.orange);
+        punkt2 = findViewById(R.id.pink);
+        bomba = findViewById(R.id.black);
 
 
         // Initial Positions
-        orange.setX(-80.0f);
-        orange.setY(-80.0f);
-        pink.setX(-80.0f);
-        pink.setY(-80.0f);
-        black.setX(-80.0f);
-        black.setY(-80.0f);
+        punkt.setX(-80.0f);
+        punkt.setY(-80.0f);
+        punkt2.setX(-80.0f);
+        punkt2.setY(-80.0f);
+        bomba.setX(-80.0f);
+        bomba.setY(-80.0f);
 
         startLabel.setVisibility(View.INVISIBLE);
         boxY = 500;
 
+
+
+
+    } else {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            action_flg = true;
+
+        } else if (event.getAction() == MotionEvent.ACTION_UP) {
+            action_flg = false;
+        }
     }
 
-    public boolean onTouchEvent(MotionEvent me) {
-        if (me.getAction() == MotionEvent.ACTION_DOWN) {
-            boxY -= 20;
+
+    }
+
+    public void changePos() {
+    if (action_flg == true) {
+        boxY -= 20;
+    }
+    else {
+        boxY += 20;
+    }
+      box.setY(boxY);
+    }
+
+
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+        if (!start_flg) {
+            start_flg = true;
+
+            // FrameHeight
+            FrameLayout frameLayout = findViewById(R.id.frame);
+            frameHeight = frameLayout.getHeight();
+
+            // Box
+            boxY = box.getY();
+            boxSize = box.getHeight();
+
+            startLabel.setVisibility(View.GONE);
+
+            timer.schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            changePos();
+                        }
+                    });
+                }
+            }, 0, 20);
+
+        } else {
+            if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                action_flg = true;
+
+            } else if (event.getAction() == MotionEvent.ACTION_UP) {
+                action_flg = false;
+            }
         }
-        box.setY(boxY);
-        return true;
+        return super.onTouchEvent(event);
     }
 }
+
